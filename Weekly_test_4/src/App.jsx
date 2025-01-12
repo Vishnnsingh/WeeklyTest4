2d381c481b89149cc9853045cd3e39a70c5e3204
vishnusingh <kumarvishnu65834@gmail.com>
2025-01-12 23:28:57 +0530

    updated

---
+++ Weekly_test_4/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import Header from './component/Header/Header'
+import './App.module.css'
+import Mainsec from './Container/Mainsec'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Mainsec />
+    </>
+  )
+}
+
+export default App
---
+++ Weekly_test_4/src/component/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from '../Header/Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={Styles.header}>Vishnu Singh Directory App</h1>
+    </div>
+  )
+}
+
+export default Header
